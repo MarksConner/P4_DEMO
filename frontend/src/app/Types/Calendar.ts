794f1b3d1058e9de76054d7290c0ebdf4515a616
ByronB <114390050+Quickkiller@users.noreply.github.com>
2025-12-12 14:02:59 -0800

    Contribution from me
    -Created contribution file
    -Also fixed export for a UI elements that was in my UI repo but not included in here for some reason. Should have no more red underlines now,

--- frontend/src/app/Types/Calendar.ts
+++ frontend/src/app/Types/Calendar.ts
@@ -3,3 +3,11 @@ export interface CalendarEvent {
   date: string;   // Use "YYYY-MM-DD" format
   title: string;
 }
+export interface DailyTimelineItem {
+  id: string;
+  startTime: string;   // "HH:MM" for now, later ISO
+  endTime?: string;    // optional
+  title: string;
+  description?: string;
+  status: "default" | "active" | "completed";
+}
